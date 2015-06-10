b9e8ecd94a63ef24b8e1ee090c12a9c5a61f4990
curlpipesh <cfl@curlpipesh.me>
2015-06-10 16:42:47 -0400

    Injector implements Opcodes to make life not be as much pain.

--- src/main/java/me/curlpipesh/bytecodetools/inject/Injector.java
+++ src/main/java/me/curlpipesh/bytecodetools/inject/Injector.java
@@ -2,6 +2,7 @@ package me.curlpipesh.bytecodetools.inject;
 
 import org.objectweb.asm.ClassReader;
 import org.objectweb.asm.ClassWriter;
+import org.objectweb.asm.Opcodes;
 import org.objectweb.asm.tree.ClassNode;
 
 import java.lang.instrument.ClassFileTransformer;
@@ -14,7 +15,7 @@ import static me.curlpipesh.bytecodetools.BytecodeTools.log;
  * @author audrey
  * @since 4/29/15
  */
-public abstract class Injector implements ClassFileTransformer {
+public abstract class Injector implements ClassFileTransformer, Opcodes {
     @Override
     @SuppressWarnings({"ConstantConditions", "unchecked"})
     public final byte[] transform(ClassLoader classLoader, String s, Class<?> aClass, ProtectionDomain protectionDomain, byte[] bytes) throws IllegalClassFormatException {
